ae26833d81110d20f525416d6e7d678df550148f
caleb1279 <caleb.dcaro@colnexsi.com.co>
2024-07-12 09:59:37 -0500

    Finalizacion del planteamiento del documento

--- getControl.cmd
+++ getControl.cmd
@@ -3,10 +3,15 @@ setlocal
 set GITVERSION=
 set GITDATE=
 set GITCREATED=
+set FRSTCMMTHASH=
 git rev-parse --short HEAD > temp.txt
 set /p GITVERSION=<temp.txt
-git log -1 --format=%%cd --date=iso > temp.txt
+git log -1 --format=%%cd --date=short > temp.txt
 set /p GITDATE=<temp.txt
+git rev-list --max-parents=0 HEAD > temp.txt
+set /p FRSTCMMTHASH=<temp.txt
+git show --date=short %FRSTCMMTHASH% --format=%%cd -s > temp.txt
+set /p GITCREATED=<temp.txt
 echo \newcommand{\gitversion}{%GITVERSION%} > gitinfo.tex
 echo \newcommand{\gitdate}{%GITDATE%} >> gitinfo.tex
 echo \newcommand{\gitcreated}{%GITCREATED%} >> gitinfo.tex
