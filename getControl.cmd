34256b4b3b0de38e956b143cb1cd55a0eb614159
caleb1279 <caleb.dcaro@colnexsi.com.co>
2024-07-12 09:29:09 -0500

    cambios de automatizacion

--- getControl.cmd
+++ getControl.cmd
@@ -1,5 +1,14 @@
 @echo off
-for /f "tokens=*" %%i in ('git rev-parse --short HEAD') do set GITVERSION=%%i
-for /f "tokens=*" %%i in ('git log -1 --format=%%cd --date=short') do set GITDATE=%%i
+setlocal
+set GITVERSION=
+set GITDATE=
+set GITCREATED=
+git rev-parse --short HEAD > temp.txt
+set /p GITVERSION=<temp.txt
+git log -1 --format=%%cd --date=iso > temp.txt
+set /p GITDATE=<temp.txt
 echo \newcommand{\gitversion}{%GITVERSION%} > gitinfo.tex
 echo \newcommand{\gitdate}{%GITDATE%} >> gitinfo.tex
+echo \newcommand{\gitcreated}{%GITCREATED%} >> gitinfo.tex
+del temp.txt
+endlocal
